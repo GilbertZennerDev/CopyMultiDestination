7cca8fa0675dced43f87e066c8886728c569961f
Your Name <you@example.com>
2025-12-07 19:35:53 +0100

    c_norminette

---
+++ c/mainFunctions.c
@@ -0,0 +1,92 @@
+#include "header.h"
+
+void	ft_folderfork(char *srcname, char *destname)
+{
+	unsigned int	i;
+	pid_t			pid;
+	char			**cmds;	
+
+	cmds = malloc(sizeof(char *) * 5);
+	cmds[0] = strdup("cp");
+	cmds[1] = strdup("-r");
+	cmds[2] = srcname;
+	cmds[3] = destname;
+	cmds[4] = NULL;
+	pid = fork();
+	if (pid == 0)
+		execvp(cmds[0], cmds);
+	else if (pid > 0)
+	{
+		free(cmds[0]);
+		free(cmds[1]);
+		free(cmds);
+	}
+}
+
+void	ft_filefork(char *srcname, char *destname)
+{
+	unsigned int	i;
+	pid_t			pid;
+	char			**cmds;	
+
+	cmds = malloc(sizeof(char *) * 4);
+	cmds[0] = strdup("cp");
+	cmds[1] = srcname;
+	cmds[2] = destname;
+	cmds[3] = NULL;
+	pid = fork();
+	if (pid == 0)
+		execvp(cmds[0], cmds);
+	else if (pid > 0)
+	{
+		free(cmds[0]);
+		free(cmds);
+	}
+}
+
+void	ft_runfork(char *srcname, char *destname, bool folder)
+{
+	if (folder)
+		ft_folderfork(srcname, destname);
+	else
+		ft_filefork(srcname, destname);
+}
+
+void	ft_loop_dests(int ac, char **av, char *srcname, bool folder)
+{
+	unsigned int	i;
+	char			*t1;
+	char			*t2;
+
+	i = 3;
+	while (i < ac)
+	{
+		if (folder)
+		{
+			t1 = strdup(av[i]);
+			t2 = strdup(av[i + 1]);
+			strcat(t1, "/");
+			strcat(t1, t2);
+			ft_runfork(srcname, t1, folder);
+			free(t1);
+			free(t2);
+			i += 2;
+		}
+		else
+		{
+			ft_runfork(srcname, av[i++], folder);
+		}
+	}
+}
+
+void	ft_loop_foldercreation(int ac, char **av)
+{
+	unsigned int	i;
+
+	i = 3;
+	while (i < ac)
+	{
+		ft_mkdir(av[i]);
+		i += 2;
+	}
+}
